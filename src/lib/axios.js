66cedebe8ccb0303814a83579ffadd2c758460b2
Lê Việt Tân <leviettan000@gmail.com>
2025-10-02 22:14:03 +0700

    fix render axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://be-mwg-electronics.onrender.com/api",
     withCredentials: true,
 });
